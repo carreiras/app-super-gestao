6a76a6a1207b44bcae977cbf2194c12c8af81e0b
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-09-23 16:57:25 -0300

    Criando o menu de navegação

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,3 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
 
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
-
-Route::get(
-    '/contato/{nome}/{categoria_id}',
-    function (
-        string $nome = 'Desconhecido',
-        int    $categoria_id = 1   // 1 - Informação
-    ) {
-        echo "Estamos aqui: $nome - $categoria_id";
-    })
-    ->where('categoria_id', '[0-9]+')
-    ->where('nome', '[A-Za-z]+');
