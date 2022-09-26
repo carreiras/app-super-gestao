09bddb9a9468a3e25045fc1fe0bd8da05e215850
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-09-26 09:30:54 -0300

    Blade - @if/@else

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,18 +1,24 @@
 <h3>Fornecedor</h3>
 
-{{-- Fica o comentário que será descartado pelo interpretador do Blade --}}
-
-{{ 'Texto de teste' }}
-<br>
-<?= 'Texto de teste';?>
-<br>
-
 @php
-    // Para comentários de uma linha
 
     /*
-     * Para comentários de multiplas linhas
+     * if () {
+     *
+     * } elseif () {
+     *
+     * } else {
+     *
+     * }
      */
-
-echo 'Texto de teste';
 @endphp
+
+{{--@dd($fornecedores)--}}
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) < 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
