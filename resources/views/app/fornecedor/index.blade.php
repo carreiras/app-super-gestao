2cc0a616968645429c9853e4410fc1372fc38e9b
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-10-02 10:44:21 -0300

    Blade - @empty

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -3,19 +3,30 @@
 @php
 
     /*
-     * if (isset(variavel)) {}  // retornar true se a variavel estiver definida
+     * if (empty(variavel)) {}  // retornar true se a variavel estiver vazia
+     * - ''
+     * - 0
+     * - 0.0
+     * - '0'
+     * - null
+     * - false
+     * - array()
+     * - $var
      */
 @endphp
 
 {{--@dd($fornecedores)--}}
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
     <br>
-    Status: {{ $fornecedores[1]['status'] }}
+    Status: {{ $fornecedores[0]['status'] }}
     <br>
-    @isset($fornecedores[1]['cnpj'])
-        Cnpf: {{ $fornecedores[1]['cnpj'] }}
+    @isset($fornecedores[0]['cnpj'])
+        Cnpf: {{ $fornecedores[0]['cnpj'] }}
+        @empty($fornecedores[0]['cnpj'])
+            - Vazio
+        @endempty
     @endisset
 @endisset
 
