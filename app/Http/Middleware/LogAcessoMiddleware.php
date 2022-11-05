0d90ed68754226c3a859e704514b4f3fdf0b6767
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-11-05 11:15:26 -0300

    Criando meu primeiro middleware

---
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use http\Env\Response;
+use Illuminate\Http\Request;
+
+class LogAcessoMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // $request - manipular
+//        return $next($request);
+        // $response - manipular
+
+        return Response('Chegamos no middleware e finalizamos no próprio middleware.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::middleware(\App\Http\Middleware\LogAcessoMiddleware::class)
+    ->get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
+    ->name('site.index');
+
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+
+
+Route::middleware(\App\Http\Middleware\LogAcessoMiddleware::class)
+    ->get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])
+    ->name('site.contato');
+
+
 Route::post('/contato', [\App\Http\Controllers\ContatoController::class, 'salvar'])->name('site.contato');
 
 Route::get('/login', function () {
